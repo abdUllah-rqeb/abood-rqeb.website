2700ad54bf04c05ee43aaee602ab5490c25c4e4c
abdUllah-rqeb <115982521+abdUllah-rqeb@users.noreply.github.com>
2022-10-17 12:02:18 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $(".icon").click(function(){
+    $("aside").animate({
+        left:"0",
+    },1100)
+    
+    });
+    });
+    
+    $(".close").click(function(){
+        $("aside").animate({
+            left:"-400px",
+        },1100)
+        
+        });
+    
+    
+    
+    
+    
+    
+    
